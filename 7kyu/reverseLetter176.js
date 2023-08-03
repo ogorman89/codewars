6684f3164923758003ced1dbbfab741d0ae23ba7
Ian O'Gorman <ogorman89@gmail.com>
2023-08-03 11:46:53 -0700

    reverse letter 176 kata

---
+++ 7kyu/reverseLetter176.js
@@ -0,0 +1,20 @@
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    return str.split('').reverse().map(x => {
+      let code = x.toLowerCase().charCodeAt()
+      return code > 96 && code < 123 ? x : ''
+    })
+    .join('')
+  }
