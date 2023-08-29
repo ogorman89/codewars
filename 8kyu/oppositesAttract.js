a5d15fcf3a884b7d6ae5c9d247aa5021ac18a092
Ian O'Gorman <ogorman89@gmail.com>
2023-08-28 21:22:20 -0700

    refactor to reduce redundant boolean

--- 8kyu/oppositesAttract.js
+++ 8kyu/oppositesAttract.js
@@ -3,5 +3,5 @@
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
 function lovefunc(flower1, flower2){
-    return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0)
+    return flower1 % 2 != flower2 % 2
   }
