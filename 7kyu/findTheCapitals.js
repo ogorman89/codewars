277fd862e84bb06889d0c9e05d03e98c5d313fa7
Ian O'Gorman <ogorman89@gmail.com>
2023-10-21 10:57:19 -0700

    find the capital letters kata

---
+++ 7kyu/findTheCapitals.js
@@ -0,0 +1,16 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+var capitals = function (word) {
+	let wordUpper = word.toUpperCase()
+  let indices = []
+  for (let i = 0; i < word.length; i++){
+    if (wordUpper[i] === word[i]){
+     indices.push(i) 
+    } 
+  }
+  return indices
+};
