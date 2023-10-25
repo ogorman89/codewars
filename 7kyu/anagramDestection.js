e5759ef92014ef9567ba6f585480e10a97536971
Ian O'Gorman <ogorman89@gmail.com>
2023-10-25 09:32:46 -0700

    refactor anagram kata

--- 7kyu/anagramDestection.js
+++ 7kyu/anagramDestection.js
@@ -10,29 +10,8 @@
 // "Buckethead" is an anagram of "DeathCubeK"
 
 // write the function isAnagram
-function newDict(word){
-    let dict = {}
-    for (let i = 0; i < word.length; i++)
-      if (word[i] in dict){
-        dict[word[i]] += 1
-      } else {
-        dict[word[i]] = 1
-      }
-    return dict
-  }
-  
-  var isAnagram = function(test, original) {
-    test = test.toLowerCase()
-    original = original.toLowerCase()
-    let testDict = newDict(test)
-    let originalDict = newDict(original)
-    //console.log(testDict)
-    //console.log(originalDict)
-    for (let key in testDict){
-      console.log(key,testDict[key],originalDict[key])
-      if (testDict[key] !== originalDict[key] || test.length !== original.length){
-        return false
-      }
-    } 
-    return true
-  };
+var isAnagram = function(test, original) {
+    test = test.toLowerCase().split('').sort().join('');
+    original = original.toLowerCase().split('').sort().join('');
+    return (test == original) ? true : false;
+};
