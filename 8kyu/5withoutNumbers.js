62b5e81fbcb8dc297325d5f0ec165a808e6038ed
Ian O'Gorman <ogorman89@gmail.com>
2023-11-08 09:48:28 -0800

    5 without numbers kata

---
+++ 8kyu/5withoutNumbers.js
@@ -0,0 +1,11 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+//always return 5 without using 0123456789*+-/
+function unusualFive() {
+    let bestRappersOfAllTime = ['Dylan','Dylan','Dylan','Dylan','Dylan']
+    return bestRappersOfAllTime.length;
+  }
