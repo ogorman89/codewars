70b4bf026e4e3f65778697828da74ade77e93561
Ian O'Gorman <ogorman89@gmail.com>
2023-12-18 10:32:38 -0800

    switcheroo kata

---
+++ 7kyu/switcheroo.js
@@ -0,0 +1,18 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x){
+    return x.split('').map(ltr => {
+      if (ltr === 'a'){
+        return 'b'
+      } else if (ltr === 'b'){
+        return 'a'
+      } else {
+        return ltr
+      }
+    }).join('')
+  }
