3c4d446325cd3f12b0abfde0fa362fe730669090
Ian O'Gorman <ogorman89@gmail.com>
2024-01-17 09:08:44 -0800

    create the rooms kata

---
+++ 8kyu/createTheRooms.js
@@ -0,0 +1,20 @@
+// You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+
+// Add rooms here
+var rooms = {
+    one: {
+    name: 'bedroom',
+    description: 'has a bed',
+    completed: false
+    },
+    two: {
+    name: 'kitchen',
+    description: 'has food',
+    completed: false
+    },
+    three: {
+    name: 'bathroom',
+    description: 'has a shower',
+    completed: false
+    }
+  }
