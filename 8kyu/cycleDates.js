626c459dc01be55aaac3d846fabd4a3c1b3e563a
Ian O'Gorman <ogorman89@gmail.com>
2024-02-03 17:03:56 -0800

    date math in unix time

--- 8kyu/cycleDates.js
+++ 8kyu/cycleDates.js
@@ -1,12 +1,12 @@
-Our function will take three parameters:
+// Our function will take three parameters:
 
-last - The Date object with the date of the last period
+// last - The Date object with the date of the last period
 
-today - The Date object with the date of the check
+// today - The Date object with the date of the check
 
-cycleLength - Integer representing the length of the cycle in days
+// cycleLength - Integer representing the length of the cycle in days
 
-Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+// Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
 
 function periodIsLate(last, today, cycleLength) {
     let diff = today.getTime() - last.getTime()
